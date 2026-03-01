2cf72193b952bc9915e1d6d8d73b26648b6b277a
Marsoupilami <mmontrichard@free.fr>
2026-03-01 15:33:51 +0100

    suppress async

--- src/range-slider-class.js
+++ src/range-slider-class.js
@@ -1,7 +1,6 @@
 import noUiSlider from "nouislider";
 
 export class RangeSliderClass extends HTMLElement {
-  
   static State = Object.freeze({
     DISCONNECTED: 0,
     CONNECTED: 1,
@@ -9,66 +8,60 @@ export class RangeSliderClass extends HTMLElement {
     OPER: 3,
     ERROR: 4
   });
-
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: "open" });
     this._initPrivateConfig();
     this._initPrivateDisplayData();
     this._debuglog("constructor");
   }
-
   setConfig(config) {
     this._debuglog("setConfig");
     if (!config.entity_min || !config.entity_max) {
       this._state = RangeSliderClass.State.ERROR;
       throw new Error("You need to define 'entity_min' and 'entity_max'");
     }
-    this._mindomain=config.entity_min.split(".")[0];
-    switch(this._mindomain) {
+    this._mindomain = config.entity_min.split(".")[0];
+    switch (this._mindomain) {
       case "number":
       case "input_number":
-        this._entitytype="number";
-        this._minservice="set_value";
+        this._entitytype = "number";
+        this._minservice = "set_value";
         break;
       case "input_datetime":
-        this._entitytype="time";
-        this._minservice="set_datetime";
+        this._entitytype = "time";
+        this._minservice = "set_datetime";
         break;
       default:
         throw new Error("Unexpected 'entity_min' domain");
     }
-
-    this._maxdomain=config.entity_max.split(".")[0];
-    switch(this._maxdomain) {
+    this._maxdomain = config.entity_max.split(".")[0];
+    switch (this._maxdomain) {
       case "number":
       case "input_number":
-        if (this._entitytype!="number") {
+        if (this._entitytype != "number") {
           throw new Error("'entity_min' and 'entity_max' shall have compatible domains");
         }
-        this._maxservice="set_value";
+        this._maxservice = "set_value";
         break;
       case "input_datetime":
-        if (this._entitytype!="time") {
+        if (this._entitytype != "time") {
           throw new Error("'entity_min' and 'entity_max' shall have compatible domains");
         }
-        this._maxservice="set_datetime";
+        this._maxservice = "set_datetime";
         break;
       default:
         throw new Error("Unexpected 'entity_max' domain");
     }
-
     this.config = config;
   }
-
   connectedCallback() {
     this._debuglog("connectedCallback");
-    if (this._state == RangeSliderClass.State.DISCONNECTED ) {
-      this._state = RangeSliderClass.State.CONNECTED
+    if (this._state == RangeSliderClass.State.DISCONNECTED) {
+      this._state = RangeSliderClass.State.CONNECTED;
       this._debuglog("CONNECTED");
     }
   }
-
   disconnectedCallback() {
     this._debuglog("disconnectedCallback");
     if (this._slider) {
@@ -76,11 +69,9 @@ export class RangeSliderClass extends HTMLElement {
     }
     this._initPrivateDisplayData();
   }
-
   set hass(hass) {
     this._debuglog("hass");
     this._hass = hass;
-
     if (this._state == RangeSliderClass.State.CONNECTED && this.config) {
       this._init();
     }
@@ -88,11 +79,9 @@ export class RangeSliderClass extends HTMLElement {
       this._updateValuesDisplay();
     }
   }
-
   _debuglog(text) {
-    if (this._activeDebug)  console.log(text);
+    if (this._activeDebug) console.log(text);
   }
-
   _initPrivateConfig() {
     this._mindomain = null;
     this._minservice = null;
@@ -101,7 +90,6 @@ export class RangeSliderClass extends HTMLElement {
     this._entitytype = null;
     this.config = null;
   }
-
   _initPrivateDisplayData() {
     this._activeDebug = false;
     this._debuglog("DISCONNECTED");
@@ -114,8 +102,7 @@ export class RangeSliderClass extends HTMLElement {
     this._lastmin = null;
     this._lastmax = null;
   }
-
-  async _init() {
+  _init() {
     this._state = RangeSliderClass.State.INIT;
     this._debuglog("INIT");
     if (this._create()) {
@@ -123,24 +110,20 @@ export class RangeSliderClass extends HTMLElement {
     } else {
       this._state = RangeSliderClass.State.ERROR;
       this._debuglog("ERROR");
-    } 
+    }
   }
-
   _renderTemplate(name) {
     throw new Error("Abstract Method");
   }
-
   _timeToMinutes(time) {
     const [hours, minutes] = time.split(":").map(Number);
     return hours * 60 + minutes;
-  };
-    
+  }
   _minutesToTime(minutes) {
-    const hours = Math.floor(minutes / 60).toString().padStart(2, '0');
-    const mins = (minutes % 60).toString().padStart(2, '0');
+    const hours = Math.floor(minutes / 60).toString().padStart(2, "0");
+    const mins = (minutes % 60).toString().padStart(2, "0");
     return `${hours}:${mins}`;
-  };
-
+  }
   _entityToSlider(value) {
     if (this._entitytype == "number") {
       return Number(value);
@@ -149,7 +132,6 @@ export class RangeSliderClass extends HTMLElement {
       return this._timeToMinutes(value);
     }
   }
-
   _sliderToDisplay(value) {
     if (this._entitytype == "number") {
       return Number(value).toFixed(1);
@@ -158,7 +140,6 @@ export class RangeSliderClass extends HTMLElement {
       return this._minutesToTime(value);
     }
   }
-
   _sliderToEntity(value) {
     if (this._entitytype == "number") {
       return Number(value);
@@ -167,51 +148,40 @@ export class RangeSliderClass extends HTMLElement {
       return this._minutesToTime(value);
     }
   }
-
   _create() {
     const {
       entity_min,
       entity_max,
       name = "range Slider"
     } = this.config;
-
     const stateMin = this._hass.states[entity_min];
     const stateMax = this._hass.states[entity_max];
-
     if (!stateMin || !stateMax) {
       this.shadowRoot.innerHTML = `<p>Entities not found</p>`;
       return false;
     }
-
     this._renderTemplate(name);
-
     this._sliderElement = this.shadowRoot.getElementById("slider");
     return true;
   }
-
-  async _updateValuesDisplay() {
+  _updateValuesDisplay() {
     if (this._updateValuesDisplayInProgress) {
       this._updateValuesDisplayRequestPending = true;
       return;
     }
     this._updateValuesDisplayInProgress = true;
-    
     try {
       if (this._userIsUpdating) {
         return;
       }
-
       const { entity_min, entity_max, unit = "" } = this.config;
-
       const minState = this._hass.states[entity_min];
       const maxState = this._hass.states[entity_max];
       if (!minState || !maxState) return;
-
       const minValue = this._entityToSlider(minState.state);
       const maxValue = this._entityToSlider(maxState.state);
-
       if (!this._slider) {
-        await this._initSlider(minValue, maxValue);
+        this._initSlider(minValue, maxValue);
         this._lastmin = minValue;
         this._lastmax = maxValue;
       } else {
@@ -225,7 +195,7 @@ export class RangeSliderClass extends HTMLElement {
       this._updateValuesDisplayInProgress = false;
       if (this._updateValuesDisplayRequestPending) {
         this._updateValuesDisplayRequestPending = false;
-        queueMicrotask(() => this._updateValuesDisplay()); // run again with last HA states
+        queueMicrotask(() => this._updateValuesDisplay());
       }
     }
     if (this._state != RangeSliderClass.State.OPER) {
@@ -234,86 +204,63 @@ export class RangeSliderClass extends HTMLElement {
     }
     return;
   }
-
-  async _initSlider( minAtInit, maxAtInit) {
+  _initSlider(minAtInit, maxAtInit) {
     if (this._slider) return;
-
     let {
       min = 0,
       max = 100,
-      step = 1,
+      step = 1
     } = this.config;
-
     if (this._entitytype == "time") {
       min = 0;
       max = 1440;
       step = 1;
     }
-
     noUiSlider.create(this._sliderElement, {
       start: [minAtInit, maxAtInit],
       connect: true,
       range: {
-        min: min,
-        max: max,
+        min,
+        max
       },
-      step: step,
+      step
     });
-
     this._slider = this._sliderElement.noUiSlider;
-
-    this._slider.on('start', () => {
+    this._slider.on("start", () => {
       this._debuglog("start");
       this._userIsUpdating = true;
     });
-
-    this._slider.on('change', (values) => {
+    this._slider.on("change", (values) => {
       this._debuglog("change");
       this._userIsUpdating = false;
       this._setEntities(values);
     });
-
-    this._slider.on('update', (values) => {
+    this._slider.on("update", (values) => {
       this._debuglog("update");
       const { unit = "" } = this.config;
       const minVal = this._sliderToDisplay(values[0]);
       const maxVal = this._sliderToDisplay(values[1]);
-
       const minElement = this.shadowRoot.getElementById("min-value");
       const maxElement = this.shadowRoot.getElementById("max-value");
-  
       minElement.textContent = `Min: ${minVal}${unit}`;
       maxElement.textContent = `Max: ${maxVal}${unit}`;
-
-      // const minPercent = ((values[0] - min) / (max - min)) * 100;
-      // const maxPercent = ((max - values[1]) / (max - min)) * 100;
-  
-      // minElement.style.left = `${minPercent}%`;
-      // maxElement.style.right = `${maxPercent}%`;
     });
-
-    this._slider.on('end', () => {
+    this._slider.on("end", () => {
       this._debuglog("end");
       this._userIsUpdating = false;
     });
-
   }
-
   _setEntities(values) {
     const { entity_min, entity_max } = this.config;
-
     const min = this._sliderToEntity(values[0]);
     const max = this._sliderToEntity(values[1]);
-
     this._hass.callService(this._mindomain, this._minservice, {
       entity_id: entity_min,
-      value: min,
+      value: min
     });
-
     this._hass.callService(this._maxdomain, this._maxservice, {
       entity_id: entity_max,
-      value: max,
+      value: max
     });
   }
-
 }
