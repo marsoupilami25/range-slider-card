bd9cb202967eb62b64a1fa5352b205d9bea0d3b6
Marsoupilami <mmontrichard@free.fr>
2026-03-01 21:53:22 +0100

    update css

--- src/range-slider-card.js
+++ src/range-slider-card.js
@@ -2,7 +2,6 @@ import { RangeSliderClass } from "./range-slider-class.js"
 import nouiCss from "nouislider/dist/nouislider.css?inline";
 
 class RangeSliderCard extends RangeSliderClass {
-  
   _renderTemplate(name) {
     this.shadowRoot.innerHTML = `
       <style>
@@ -11,11 +10,20 @@ class RangeSliderCard extends RangeSliderClass {
           display: flex;
           flex-direction: column;
           align-items: center;
-          padding: 8px;
+          gap: 12px;
+          background: var(--ha-card-background, var(--card-background-color));
+          border-radius: var(--ha-card-border-radius, 12px);
+          box-shadow: var(--ha-card-box-shadow, 0px 2px 4px rgba(0, 0, 0, 0.16));
+          border-width: var(--ha-card-border-width, 1px);
+          border-style: solid;
+          border-color: var(--ha-card-border-color, var(--divider-color));
+          transition: all 0.3s ease-out;
+          box-sizing: border-box;
         }
         .slider {
           width: 90%;
-          margin: 8px 0;
+          margin: 0px 10px;
+          height: 100%;
         }
         .values {
           display: flex;
@@ -35,6 +43,34 @@ class RangeSliderCard extends RangeSliderClass {
         #max-value {
           color: var(--primary-text-color);
         }
+        .noUi-target {
+          background: color-mix(in srgb, var(--disabled-color) 30%, transparent);
+          border-radius: 10px / 16px;
+          border: none;
+          box-shadow: none;
+        }
+        .noUi-base {
+          height: 16px;
+        }
+        .noUi-connect {
+          height: 16px;
+          background: color-mix(in srgb, var(--primary-color) 30%, transparent);
+        }
+        .noUi-horizontal .noUi-handle {
+          width: 18px;
+          height: 18px;
+          top: -1px;
+          right: -9px;
+          background: var(--primary-text-color);
+          border-width: 3px;
+          border-style: solid;
+          border-color: var(--primary-color);
+          border-radius: 15px;
+          box-shadow: none;
+        }
+        .noUi-handle::before, .noUi-handle::after {
+          display: none;
+        }
       </style>
       <div class="container">
         <div class="title">${name}</div>
@@ -46,11 +82,9 @@ class RangeSliderCard extends RangeSliderClass {
       </div>
     `;
   }
-
   getCardSize() {
     return 2;
   }
-
 }
 
 customElements.define('range-slider-card', RangeSliderCard);
